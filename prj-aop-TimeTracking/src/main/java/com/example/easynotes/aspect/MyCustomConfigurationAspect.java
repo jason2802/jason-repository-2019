630f6561986fbc128a21d5020a599fcf3a049972
jason2802 <fako02@yahoo.com>
2019-08-04 22:25:21 +0200

    Add files via upload

---
+++ prj-aop-TimeTracking/src/main/java/com/example/easynotes/aspect/MyCustomConfigurationAspect.java
@@ -0,0 +1,54 @@
+package com.example.easynotes.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+
+/*
+ * https://dzone.com/articles/implementing-aop-with-spring-boot-and-aspectj
+ */
+
+@Aspect
+@Configuration
+public class MyCustomConfigurationAspect {
+
+	private static final Logger LOGGER = LogManager.getLogger( MyCustomConfigurationAspect.class );
+	
+	@Around("@annotation(com.example.easynotes.aspect.TimeTracking)")	
+	public void around(ProceedingJoinPoint joinPoint ) throws Throwable {
+		LOGGER.debug( " ---------- Inside around ------------ ");
+		long startTime = System.currentTimeMillis();
+		
+		joinPoint.proceed();
+		
+		long timeTaking = System.currentTimeMillis() - startTime;
+		LOGGER.debug( "  ---------- Time taken by {} is {} ", joinPoint, timeTaking );				
+	}
+	
+	// @Before("execution(* com.example.easynotes.aspect.UserAccess.*.*(..))")
+	@Before("@annotation(com.example.easynotes.aspect.UserAccess)")	
+	public void before(JoinPoint joinPoint) throws Throwable {
+		LOGGER.debug( " +++++++++ get Inside before getUserAccess ------------ ");
+		System.out.println(" +++++++++ hijacked : " + joinPoint.getSignature().getName() );
+		LOGGER.debug( " +++++++++ get Outside before getUserAccess ------------ ");
+	}
+	
+}
+
+/*
+ 
+Notes:
+
+@Around uses an around advice. It intercepts the method call and uses joinPoint.proceed() to execute the method.
+@annotation(com.in28minutes.springboot.tutorial.basics.example.aop.TrackTime) is the pointcut to define interception based on an annotation — @annotation followed by the complete type name of the annotation.
+Once we define the annotation and the advice, we can use the annotation on methods that we would want to track, as shown below:
+
+*/
